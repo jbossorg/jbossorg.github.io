36e9e03ce6c9b7f461036783863810074985710e
Luke <ldary@redhat.com>
2020-08-05 09:21:24 -0400

    Update document.js

--- src/templates/document.js
+++ src/templates/document.js
@@ -10,8 +10,8 @@ module.exports = ({node}) => `
     https://kit-free.fontawesome.com/ https://cdnjs.cloudflare.com https://static.redhat.com https://static.hotjar.com https://in.ml314.com 'unsafe-inline' 'unsafe-eval'; 
     style-src https: 'unsafe-inline'; 
     img-src 'self' https://developers.redhat.com https://consent.trustarc.com https://smetrics.jboss.org 
-    https://s1795.t.eloqua.com https://cm.everesttech.net https://ds-aksb-a.akamaihd.net
-    https://www.google-analytics.com/ https://stats.g.doubleclick.net https://px.ads.linkedin.com; 
+    https://s1795.t.eloqua.com https://cm.everesttech.net https://ds-aksb-a.akamaihd.net https://www.google.com
+    https://www.google-analytics.com/ https://stats.g.doubleclick.net https://px.ads.linkedin.com https://p.adsymptotic.com; 
     frame-src https://redhat.demdex.net/;
     connect-src https://dpm.demdex.net https://redhat.tt.omtrdc.net https://dcp2.jboss.org;
     font-src https://fonts.googleapis.com/ https://fonts.gstatic.com/ https://consent.trustarc.com">
